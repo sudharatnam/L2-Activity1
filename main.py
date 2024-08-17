4e49b9ef212382a626a3894e749b024cecc29cc0
sudharatnam <suda.dol@gmail.com>
2024-08-17 22:17:52 +0530

    datatype

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Hello World")
+name="cody"
+age=10
+weight=25.6
+Is_Student=True
+print("Name :",name)
+print("Type of name is:",type(name))
+print("Age:",age)
+print("Type of age is:",type(age))
